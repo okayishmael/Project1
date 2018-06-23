1880181f566a16882ae26f97ba0a8d3ac1b96c10
NAME <EMAIL>
2018-06-23 14:40:42 -0400

    added css and modified

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-//Author: Ishmael Sunday | github.com/okayishmael
-//Code 201 Project @ Code Partners > https://codepartners.net
+//Author:Ishmael Sunday | github.com/okayishmael
+//Code 201 Project1 @ Code Partners > https://codepartners.net
 
 
-let name = prompt("What\'s my name?"); //declare var use prompt to display value in browser and collect user input
+let name = prompt("What\'s your name?"); //declare var use prompt to display value in browser and collect user input
 let origin = prompt("Where are you from?"); // Same here
-let coder = confirm('Click "OK" you are a coder?'); // declare var confirm to collect user input
+let coder = confirm('Click "OK" if you are a coder?'); // declare var confirm to collect user input
 console.log(); // Conprint
 
