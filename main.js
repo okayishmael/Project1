f516dc7c0a27ce428ff9a081dc77c18f0e47c543
NAME <EMAIL>
2018-06-27 18:56:44 -0400

    made updates to main.js

--- main.js
+++ main.js
@@ -4,9 +4,11 @@
 
 let name = prompt("What\'s your name?"); //declare var use prompt to display value in browser and collect user input
 if (name != null){
-alert("Hi "+name+"! Please click the \"OK button\" to enter your current city.")}; // using alert function. no input
-let origin = prompt("Enter your current City:"); // prompt, input require
-let gender = prompt("Gender:");// prompt, input require
-let coder = confirm('Click "OK" if you are a coder.'); // declare var confirm to collect user input
-console.log(); // printing place function name in the paranthesis 
-
+alert("Hi " + name + "! Please click the \"OK button\" to enter your current city.")}; // using alert function. no input
+let origin = prompt(name + " Enter your current City:"); // prompt, input require
+let gender = prompt(name + " please enter your Gender:");// prompt, input require
+let coder = confirm(name + ' Click "OK" if you are a coder.'); // declare var confirm to collect user input
+console.log(name); // printing place function name in the paranthesis 
+console.log(origin); // printing place function name in the paranthesis
+console.log(gender); // printing place function name in the paranthesis
+console.log(coder); // printing place function name in the paranthesis
